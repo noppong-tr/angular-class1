8c4c7af42e4e476ef78d7e4eb1e3865a9311338a
Noppong Trong <noppong.tr@inet.co.th>
2019-10-08 18:58:25 +0700

    start angular 08-10-2019

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'productlist',
+    pathMatch: 'full',
+  },
+  {
+    path: 'productlist',
+    component: ProductListComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
